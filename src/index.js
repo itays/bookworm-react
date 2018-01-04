15a93da9e3452eaae837dbf702af7078131a84c5
Itay Sidis <itaysidis@gmail.com>
2018-01-04 18:39:36 +0200

    ➕ Adding a dependency
    Redux setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import App from "./App";
+import rootReducer from "./rootReducer";
 
-import App from './App';
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
-
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
---
+++ src/rootReducer.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+export default combineReducers({
+  user: () => ({})
+});
